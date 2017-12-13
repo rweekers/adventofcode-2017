66aa69bd393c40a5f50d1b18c0fe0b1ec79337e5
Remco Weekers <rweekers@bol.com>
2017-12-13 22:24:47 +0100

    Add spiral logic, some fixing and cleaning to do

--- three/exercise-3.js
+++ three/exercise-3.js
@@ -1,12 +1,39 @@
-
 const Rx = require('rxjs/Rx');
+const Grid = require('./grid');
 
 const number$ = Rx.Observable.range(0, 289326 + 1).skip(1);
 
 number$
-   .scan((acc, curr) => curr)
+   .scan((acc, curr) => doMove(acc), new Grid()).take(4)
+   .map(grid => grid.getX() + ', ' + grid.getY())
    .subscribe(console.log);
 
-function checkMoveDirection(a, b) {
-	return -1;
+function doMove(grid) {
+    // default movement is to the right, always try to turn left
+    console.log('right: ' + grid.right() + ' left: ' + grid.left() + ' up: ' + grid.up() + ' down: ' + grid.down());
+
+
+    // initial movement
+    if (grid.getX() === 0 && grid.getY() === 0) {
+        return grid.moveRight();
+    }
+
+    // algorithm for moving
+    if (grid.right() == true) {
+        // right is occupied, go downwards
+        return grid.moveDown();
+    } else {
+        // right is free, first try upwards and then leftwards
+        if(grid.up() == true) {
+            // up is occupied, go right
+            return grid.moveRight();
+        } else {
+            if (grid.left() == true) {
+                // go down, right and up are both occupied
+                return grid.moveUp();
+            } else {
+                return grid.moveLeft();
+            }
+        }
+    }
 }
---
+++ three/grid.js
@@ -0,0 +1,55 @@
+module.exports = class Grid {
+    
+        constructor(x, y, grid) {
+            this.x = x || 0;
+            this.y = y || 0;
+            this.grid = grid || new Map().set(0, new Map().set(0, 0));
+            this.grid.set(this.x, new Map().set(this.y, true));
+        }
+
+        left() {
+            return this.grid.has(parseInt(this.x - 1)) && this.grid.get(parseInt(this.x - 1)).has(parseInt(this.y));
+        }
+
+        right() {
+            return this.grid.has(parseInt(this.x + 1)) && this.grid.get(parseInt(this.x + 1)).has(parseInt(this.y));
+        }
+
+        up() {
+            return this.grid.has(parseInt(this.x)) && this.grid.get(parseInt(this.x)).has(parseInt(this.y - 1));
+        }
+
+        down() {
+            return this.grid.has(parseInt(this.x)) && this.grid.get(parseInt(this.x)).has(parseInt(this.y + 1));
+        }
+    
+        moveLeft() {
+            console.log('*left*');
+            return new Grid(parseInt(this.x - 1), parseInt(this.y), this.grid);
+        }
+
+        moveRight() {
+            console.log('*right*');
+            return new Grid(parseInt(this.x + 1), parseInt(this.y), this.grid);
+        }
+
+        moveUp() {
+            console.log('*up*');
+            return new Grid(parseInt(this.x), parseInt(this.y - 1), this.grid);
+        }
+
+        moveDown() {
+            console.log('*down*');
+            return new Grid(parseInt(this.x), parseInt(this.y + 1), this.grid);
+        }
+
+        getX() {
+            return this.x;
+        }
+
+        getY() {
+            return this.y;
+        }
+
+    }
+    
