e114a2e763760e07ae322c0ec35857ffd596558a
Remco Weekers <remco@craftsmen.nl>
2017-12-17 09:12:01 +0100

    Simple kotlin setup

--- kotlin/src/main/java/nl/orangeflamingo/adventofcode2017/four/Exercise4.kt
+++ kotlin/src/main/java/nl/orangeflamingo/adventofcode2017/four/Exercise4.kt
@@ -1,5 +1,13 @@
 package nl.orangeflamingo.adventofcode2017.four
 
-fun exercise4(file: String) {
-    println("hoi")
+class Exercise4() {
+
+    fun exercise4(file: String) {
+        println(file)
+    }
+}
+
+fun main(args: Array<String>) {
+    val exc4 = Exercise4();
+    exc4.exercise4("tbd");
 }
--- kotlin/src/test/java/nl/orangeflamingo/adventofcode2017/four/Exercise4Test.kt
+++ kotlin/src/test/java/nl/orangeflamingo/adventofcode2017/four/Exercise4Test.kt
@@ -8,6 +8,8 @@ class Exercise4Test {
 
     @Test
     fun exercise4SilverTest() {
+        val exc4 = Exercise4();
+        exc4.exercise4("tbd");
         Assert.assertTrue(1 == 1)
     }
 
