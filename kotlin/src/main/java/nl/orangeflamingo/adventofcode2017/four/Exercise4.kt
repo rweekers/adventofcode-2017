a5d3a6b30039b7213dc57d638f15707b2ddf5e30
Remco Weekers <remco.weekers@gmail.com>
2017-12-19 22:47:21 +0100

    Add function for comparing anagrams

--- kotlin/src/main/java/nl/orangeflamingo/adventofcode2017/four/Exercise4.kt
+++ kotlin/src/main/java/nl/orangeflamingo/adventofcode2017/four/Exercise4.kt
@@ -17,10 +17,14 @@ class Exercise4() {
         return correctPassphrases
     }
 
-    fun isValidPassphrase(line: String): Boolean {
+    private fun isValidPassphrase(line: String): Boolean {
         val words = line.split(" ")
 
         val wordStream = words.toObservable()
+
+        // TODO
+        val distinctGoldWordStream = wordStream;
+
         val distinctWordStream = words.toObservable().distinct()
         var originalLenght = 0
         var distinctLength = 0
@@ -29,6 +33,17 @@ class Exercise4() {
         distinctWordStream.reduce(0, {acc, _ -> acc + 1}).subscribe({a -> distinctLength = a})
 
         return(originalLenght == distinctLength)
+    }
+
+    private fun isWordEqual(word1: String, word2: String): Boolean {
+        if (word1.length != word2.length) return false
+
+        var wordList1 : List<Char> = word1.toList()
+        var wordList2: MutableList<Char> = word2.toMutableList()
+
+        wordList1.forEach({c -> if (wordList2.contains(c)) wordList2.remove(c) else return false })
+
+        return true;
     }
 }
 
