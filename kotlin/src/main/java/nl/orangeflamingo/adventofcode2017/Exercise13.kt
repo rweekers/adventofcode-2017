7917a467bd46858c92236907080a6504cc8df841
Remco Weekers <remco.weekers@gmail.com>
2018-01-15 23:41:27 +0100

    Exercise 13 first setup

---
+++ kotlin/src/main/java/nl/orangeflamingo/adventofcode2017/Exercise13.kt
@@ -0,0 +1,17 @@
+package nl.orangeflamingo.adventofcode2017
+
+class Exercise13(fileName: String) {
+
+    fun silverExercise13(): Int {
+        return 0
+    }
+
+    init {
+        println("Parsing $fileName")
+    }
+}
+
+fun main(args: Array<String>) {
+    val answerSilver = Exercise13("/input/input13.txt").silverExercise13()
+    println("The answer for the silver exercise is: $answerSilver")
+}
---
+++ kotlin/src/test/java/nl/orangeflamingo/adventofcode2017/Exercise13Test.kt
@@ -0,0 +1,15 @@
+package nl.orangeflamingo.adventofcode2017
+
+import org.junit.Assert
+import org.junit.Test
+
+
+class Exercise13Test {
+
+    private val exc13 = Exercise13("/test/thirteen/test13.txt")
+
+    @Test
+    fun exercise13SilverTest() {
+        Assert.assertEquals(0, exc13.silverExercise13())
+    }
+}
