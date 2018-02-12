03601e99b6a70ab4c95616fc34572a0966ce43ee
Remco Weekers <rweekers@bol.com>
2018-02-12 17:36:20 +0100

    Add silver solution

--- kotlin/src/main/java/nl/orangeflamingo/adventofcode2017/Exercise17.kt
+++ kotlin/src/main/java/nl/orangeflamingo/adventofcode2017/Exercise17.kt
@@ -4,17 +4,17 @@ class Exercise17(stepsForward: Int) {
 
     val spinlock = SpinLock(stepsForward)
 
-    fun silverExercise16(): Int {
+    fun silverExercise17(): Int {
         return processMoves()
     }
 
     private fun processMoves(): Int {
         (1..2017).forEach { spinlock.spin(it) }
-        return 0
+        return spinlock.getNextValue()
     }
 }
 
-class SpinLock(val stepsForward: Int, private val circularBuffer: MutableList<Int> = mutableListOf()) {
+class SpinLock(private val stepsForward: Int, private val circularBuffer: MutableList<Int> = mutableListOf()) {
 
     private var index = 0
 
@@ -23,20 +23,29 @@ class SpinLock(val stepsForward: Int, private val circularBuffer: MutableList<In
     }
 
     fun spin(value: Int) {
-        println("spinning...")
         index = getCircularIndex(index + stepsForward)
-        circularBuffer.add(index, value)
+        if (index == circularBuffer.size - 1) {
+            circularBuffer.add(value)
+            index = circularBuffer.size - 1
+        } else {
+            index += 1
+            circularBuffer.add(index, value)
+        }
     }
 
     private fun getCircularIndex(newIndex: Int): Int {
-        return (circularBuffer.size % newIndex) - 1
+        return (newIndex % circularBuffer.size )
+    }
+
+    fun getNextValue(): Int {
+        return circularBuffer.get(circularBuffer.indexOf(2017) + 1)
     }
 
 }
 
 fun main(args: Array<String>) {
-    val stepsForward = 3
+    val stepsForward = 314
     val exc17 = Exercise17(stepsForward)
-    val answerSilver = exc17.silverExercise16()
+    val answerSilver = exc17.silverExercise17()
     println("The answer for the silver exercise is: $answerSilver")
 }
---
+++ kotlin/src/test/java/nl/orangeflamingo/adventofcode2017/Exercise17Test.kt
@@ -0,0 +1,22 @@
+package nl.orangeflamingo.adventofcode2017
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class Exercise17Test {
+
+    private val testpuzzleInput = 3
+    private val actualPuzzleInput = 314
+
+    @Test
+    fun `test input silver`() {
+        val exc17 = Exercise17(testpuzzleInput)
+        assertEquals(638, exc17.silverExercise17())
+    }
+
+    @Test
+    fun `actual input silver`() {
+        val exc17 = Exercise17(actualPuzzleInput)
+        assertEquals(355, exc17.silverExercise17())
+    }
+}
