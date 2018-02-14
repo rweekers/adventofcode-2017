1ff7918e57026020e4d34b9a6a58e8d714b05afa
Remco Weekers <rweekers@bol.com>
2018-02-14 13:29:12 +0100

    Exercise 18 silver

--- kotlin/src/main/java/nl/orangeflamingo/adventofcode2017/Exercise18.kt
+++ kotlin/src/main/java/nl/orangeflamingo/adventofcode2017/Exercise18.kt
@@ -5,6 +5,10 @@ import java.io.InputStream
 class Exercise18(fileName: String) {
 
     private val instructions: List<Instruction> = parseInput(fileName)
+    private val register: MutableMap<Char, Long> = mutableMapOf()
+    private var recoveredFrequency: Long = 0
+    private var foundFrequency: Long = 0
+    private var index = 0
 
     private fun linesAsList(file: String): MutableList<String> {
         val inputStream: InputStream = this.javaClass.getResource(file).openStream()
@@ -14,17 +18,69 @@ class Exercise18(fileName: String) {
         return lineList
     }
 
-    fun silverExercise18(): Int {
-        return 0
+    fun silverExercise18(): Long {
+        processInstructions()
+        return foundFrequency
     }
 
     private fun processInstructions() {
-        return instructions.forEach{ processInstruction(it)}
+        while (recoveredFrequency == 0L) {
+            processInstruction(instructions[index])
+        }
+    }
+
+    private fun setRegisterValue(registerKey: Char, value: Long) {
+        register[registerKey] = value
+    }
+
+    private fun getRegisterValue(registerKey: Char): Long {
+        return register.computeIfAbsent(registerKey, { _ -> 0 })
     }
 
     private fun processInstruction(instruction: Instruction) {
         when (instruction) {
-            is SetValue -> ""
+            is SetValue -> {
+                setRegisterValue(instruction.register, instruction.value)
+                index++
+            }
+            is SetRegister -> {
+                setRegisterValue(instruction.register, getRegisterValue(instruction.value))
+                index++
+            }
+            is Sound -> {
+                foundFrequency = getRegisterValue(instruction.register)
+                index++
+            }
+            is AddValue -> {
+                setRegisterValue(instruction.register, getRegisterValue(instruction.register) + instruction.value)
+                index++
+            }
+            is AddRegister -> {
+                setRegisterValue(instruction.register, getRegisterValue(instruction.register) + getRegisterValue(instruction.value))
+                index++
+            }
+            is MultiplyValue -> {
+                setRegisterValue(instruction.register, getRegisterValue(instruction.register) * instruction.multiplier)
+                index++
+            }
+            is MultiplyRegister -> {
+                setRegisterValue(instruction.register, getRegisterValue(instruction.register) * getRegisterValue(instruction.multiplier))
+                index++
+            }
+            is ModuloValue -> {
+                setRegisterValue(instruction.register, getRegisterValue(instruction.register) % instruction.divider)
+                index++
+            }
+            is ModuloRegister -> {
+                setRegisterValue(instruction.register, getRegisterValue(instruction.register) % getRegisterValue(instruction.divider))
+                index++
+            }
+            is Recover -> {
+                if (getRegisterValue(instruction.register) > 0) recoveredFrequency = getRegisterValue(instruction.register)
+                index++
+            }
+            is JumpValue -> if (getRegisterValue(instruction.register) > 0) index += instruction.offset.toInt() else index++
+            is JumpRegister -> if (getRegisterValue(instruction.register) > 0) index += getRegisterValue(instruction.offset).toInt() else index++
         }
     }
 
@@ -37,44 +93,48 @@ class Exercise18(fileName: String) {
             val parts = regExp2.findAll(it).toList().map { it.value }
 
             when (parts[0]) {
-                "set" -> if (parts[2].toIntOrNull() != null)SetValue(parts[1], parts[2].toInt()) else SetRegister(parts[1], parts[2])
-                "snd" -> Sound(parts[1])
-                "add" -> if (parts[2].toIntOrNull() != null)AddValue(parts[1], parts[2].toInt()) else AddRegister(parts[1], parts[2])
-                "mul" -> if (parts[2].toIntOrNull() != null)MultiplyValue(parts[1], parts[2].toInt()) else MultiplyRegister(parts[1], parts[2])
-                "mod" -> if (parts[2].toIntOrNull() != null)ModuloValue(parts[1], parts[2].toInt()) else ModuloRegister(parts[1], parts[2])
-                "rec" -> Recover(parts[1])
-                "jgz" -> if (parts[2].toIntOrNull() != null)JumpValue(parts[1], parts[2].toInt()) else JumpRegister(parts[1], parts[2])
-                else -> Sound(parts[1])
+                "set" -> if (parts[2].toLongOrNull() != null) SetValue(parts[1].toFirstChar(), parts[2].toLong()) else SetRegister(parts[1].toFirstChar(), parts[2].toFirstChar())
+                "snd" -> Sound(parts[1].toFirstChar())
+                "add" -> if (parts[2].toLongOrNull() != null) AddValue(parts[1].toFirstChar(), parts[2].toLong()) else AddRegister(parts[1].toFirstChar(), parts[2].toFirstChar())
+                "mul" -> if (parts[2].toLongOrNull() != null) MultiplyValue(parts[1].toFirstChar(), parts[2].toLong()) else MultiplyRegister(parts[1].toFirstChar(), parts[2].toFirstChar())
+                "mod" -> if (parts[2].toLongOrNull() != null) ModuloValue(parts[1].toFirstChar(), parts[2].toLong()) else ModuloRegister(parts[1].toFirstChar(), parts[2].toFirstChar())
+                "rcv" -> Recover(parts[1].toFirstChar())
+                "jgz" -> if (parts[2].toLongOrNull() != null) JumpValue(parts[1].toFirstChar(), parts[2].toLong()) else JumpRegister(parts[1].toFirstChar(), parts[2].toFirstChar())
+                else -> Sound(parts[1].toFirstChar())
             }
         }
+    }
+
+    private fun String.toFirstChar(): Char {
+        return this.toCharArray()[0]
     }
 }
 
 sealed class Instruction
 
-class SetValue(val register: String, val value: Int) : Instruction()
+class SetValue(val register: Char, val value: Long) : Instruction()
 
-class SetRegister(val register: String, val value: String) : Instruction()
+class SetRegister(val register: Char, val value: Char) : Instruction()
 
-class Sound(val register: String) : Instruction()
+class Sound(val register: Char) : Instruction()
 
-class AddValue(val register: String, val value: Int) : Instruction()
+class AddValue(val register: Char, val value: Long) : Instruction()
 
-class AddRegister(val register: String, val value: String) : Instruction()
+class AddRegister(val register: Char, val value: Char) : Instruction()
 
-class MultiplyValue(val register: String, val multiplier: Int) : Instruction()
+class MultiplyValue(val register: Char, val multiplier: Long) : Instruction()
 
-class MultiplyRegister(val register: String, val multiplier: String) : Instruction()
+class MultiplyRegister(val register: Char, val multiplier: Char) : Instruction()
 
-class ModuloValue(val register: String, val divider: Int) : Instruction()
+class ModuloValue(val register: Char, val divider: Long) : Instruction()
 
-class ModuloRegister(val register: String, val divider: String) : Instruction()
+class ModuloRegister(val register: Char, val divider: Char) : Instruction()
 
-class Recover(val register: String) : Instruction()
+class Recover(val register: Char) : Instruction()
 
-class JumpValue(val register: String, val offset: Int) : Instruction()
+class JumpValue(val register: Char, val offset: Long) : Instruction()
 
-class JumpRegister(val register: String, val offset: String) : Instruction()
+class JumpRegister(val register: Char, val offset: Char) : Instruction()
 
 fun main(args: Array<String>) {
     val exc18 = Exercise18("/input/input18.txt")
--- kotlin/src/test/java/nl/orangeflamingo/adventofcode2017/Exercise18Test.kt
+++ kotlin/src/test/java/nl/orangeflamingo/adventofcode2017/Exercise18Test.kt
@@ -8,12 +8,12 @@ class Exercise18Test {
     @Test
     fun `test input silver`() {
         val exc18 = Exercise18("/test/eightteen/test18.txt")
-        assertEquals(0, exc18.silverExercise18())
+        assertEquals(4, exc18.silverExercise18())
     }
 
     @Test
     fun `actual input silver`() {
         val exc18 = Exercise18("/input/input18.txt")
-        assertEquals(0, exc18.silverExercise18())
+        assertEquals(7071, exc18.silverExercise18())
     }
 }
