be0d7477b5bb6ea3f75618a0dacab4a3538b337c
Remco Weekers <remco@craftsmen.nl>
2018-03-18 08:57:10 +0100

    Exercise 25

--- kotlin/src/main/java/nl/orangeflamingo/adventofcode2017/Exercise25.kt
+++ kotlin/src/main/java/nl/orangeflamingo/adventofcode2017/Exercise25.kt
@@ -14,8 +14,10 @@ class Exercise25(fileName: String) {
             .map { it.split(" ").last().dropLast(1) }
             .chunked(9)
             .map {
-                it[0] to mapOf(it[1].toInt() to Rule(it[2].toInt(), it[3], it[4]), it[5].toInt() to Rule(it[6].toInt(), it[7], it[8]))
-            }
+                it[0].first() to mapOf(it[1].toInt() to Rule(it[2].toInt(), it[3], it[4].first()), it[5].toInt() to Rule(it[6].toInt(), it[7], it[8].first()))
+            }.toMap()
+    
+    val turingMachine = TuringMachine(ruleMap, steps, initialState)
 
     private fun linesAsList(file: String): MutableList<String> {
         val inputStream: InputStream = this.javaClass.getResource(file).openStream()
@@ -26,21 +28,41 @@ class Exercise25(fileName: String) {
     }
 
     fun silverExercise25(): Int {
-        return 0
-    }
-
-    fun goldExercise25(): Int {
-        return 0
+        return turingMachine.run()
     }
 }
 
-class Rule(val value: Int, val action: String, val nextState: String)
+class Rule(val value: Int, val action: String, val nextState: Char)
 
 fun main(args: Array<String>) {
     val exc25Silver = Exercise25("/input/input25.txt")
     val answerSilver = exc25Silver.silverExercise25()
     println("The answer for the silver exercise is: $answerSilver")
-    val exc25Gold = Exercise25("/input/input25.txt")
-    val answerGold = exc25Gold.goldExercise25()
-    println("The answer for the silver exercise is: $answerGold")
+}
+
+class TuringMachine(private val ruleMap: Map<Char,Map<Int, Rule>>, private val steps: Int, private var state: Char) {
+    private val tape = mutableMapOf<Int, Int>()
+    private var cursor = 0
+
+    fun run(): Int {
+        repeat(steps) {
+            execute()
+        }
+        return tape.filter { it -> it.value == 1 }.size
+    }
+
+    private fun execute() {
+        val tapeValue = tape.getOrElse(cursor, { 0 })
+        val rule = ruleMap[state]!![tapeValue]!!
+        handleRule(rule)
+    }
+
+    private fun handleRule(rule: Rule) {
+        tape.put(cursor, rule.value)
+        when (rule.action) {
+            "right" -> cursor++
+            "left" -> cursor--
+        }
+        state = rule.nextState
+    }
 }
--- kotlin/src/test/java/nl/orangeflamingo/adventofcode2017/Exercise25Test.kt
+++ kotlin/src/test/java/nl/orangeflamingo/adventofcode2017/Exercise25Test.kt
@@ -14,18 +14,6 @@ class Exercise25Test {
     @Test
     fun `actual input silver`() {
         val exc25 = Exercise25("/input/input25.txt")
-        assertEquals(0, exc25.silverExercise25())
-    }
-
-    @Test
-    fun `test input gold`() {
-        val exc25 = Exercise25("/test/twenty_five/test25.txt")
-        assertEquals(0, exc25.goldExercise25())
-    }
-
-    @Test
-    fun `actual input gold`() {
-        val exc25 = Exercise25("/input/input25.txt")
-        assertEquals(0, exc25.goldExercise25())
+        assertEquals(4230, exc25.silverExercise25())
     }
 }
